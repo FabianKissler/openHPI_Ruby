9dbd4649b6f86db28f68ffd179fadb502a243d8a
Kissler, Fabian <fabian.kissler@sap.com>
2018-10-31 14:02:15 +0100

    Added 02_04_modules.rb

---
+++ week_02/exercises/02_04_modules.rb
@@ -0,0 +1,23 @@
+# openHPI - Programmieren mit Ruby
+# 2.4 Modules
+
+class Task
+    attr_accessor :title    
+    
+    include Comparable
+    
+    def initialize(title)
+        @title = title
+    end
+
+    #Compare by lowercase name
+    def <=>(other)
+        title.downcase <=> other.title.downcase
+    end
+end
+
+Task.new("Learn Ruby").respond_to? :between?
+task1 = Task.new("Learn Ruby") 
+task2 = Task.new("get certificate")
+
+puts task1 > task2
