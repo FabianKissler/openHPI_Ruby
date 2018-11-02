4fd3d6f84d18f8e71df08b20be374c07370bb381
Kissler, Fabian <fabian.kissler@sap.com>
2018-11-02 16:10:21 +0100

    Added Homework for week 2 part 2

---
+++ week_02/exercises/homework/part_02/tasks.rb
@@ -0,0 +1,97 @@
+# Für Time.parse
+require "time"
+
+
+class Task
+  def initialize(text, start_time, end_time = nil)
+    @text = text
+    @start_time = start_time
+    @end_time = end_time
+  end
+
+  def self.parse(line)
+    parts = line.strip.split(",")
+    new(
+      parts[0],
+      Time.parse(parts[1]),
+      (Time.parse(parts[2]) rescue nil)
+    )
+  end
+
+  attr_reader :text
+
+  def completed?
+    @end_time
+  end
+
+  def complete!
+    @end_time = Time.new
+  end
+
+  def duration
+    return 0 unless completed?
+
+    (@end_time - @start_time).to_i
+  end
+end
+
+
+class TaskList
+  def initialize(tasks = [])
+    @tasks = tasks
+  end
+
+  def self.from_file(filename)
+    instance = new
+    instance.load(filename)
+    instance
+  end
+
+  def load(filename)
+    File.open(filename, "r") do |file|
+      file.each do |line|
+        self << Task.parse(line)
+      end
+    end
+  end
+
+  include Enumerable
+
+  def each
+    @tasks.each { |t| yield t }
+  end
+
+  def <<(task)
+    @tasks << task
+    self
+  end
+
+  def size
+    @tasks.size
+  end
+
+  def num_completed
+    @tasks.select(&:completed?).size
+  end
+
+  def num_open
+    @tasks.reject(&:completed?).size
+  end
+
+  def print_statistics
+    print self.size, " Aufgaben im System\n"
+    print self.num_open, " offen, ", self.num_completed, " erledigt\n"
+    @tasks.reject(&:completed?).each do |task|
+      print "OFFEN: ", task.text, "\n"
+    end
+    @tasks.select(&:completed?).each do |task|
+      print "ERLEDIGT: ", task.text, "\n"
+    end
+  end
+end
+
+case ARGV.shift
+when "list"
+  list = TaskList.from_file "tasks.txt"
+  list.print_statistics
+end
